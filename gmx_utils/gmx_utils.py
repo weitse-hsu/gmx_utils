d07e743ad14286c7e5035c56d52e9b56b97cd49f
wehs7661 <wehs7661@colorado.edu>
2026-01-07 12:08:30 +0000

    Added gmx_utils.py

---
+++ gmx_utils/gmx_utils.py
@@ -0,0 +1,24 @@
+import subprocess
+
+
+def run_gmx_cmd(arguments, prompt_input=None):
+    try:
+        result = subprocess.run(
+            arguments,
+            stdout=subprocess.PIPE,    # Capture stdout
+            stderr=subprocess.STDOUT,  # Redirect stderr to stdout
+            text=True,                 # Return strings instead of bytes
+            input=prompt_input,
+            check=True                 # Raise exception on non-zero exit
+        )
+        returncode, stdout = result.returncode, result.stdout
+    except subprocess.CalledProcessError as e:
+        # e.output contains the full error message (thanks to stderr=STDOUT)
+        returncode, stdout = e.returncode, e.output
+
+    if returncode != 0:
+        raise RuntimeError(f'{" ".join(arguments[:2])} failed with return code {returncode}:\n{stdout}')
+    else:
+        print(f"\n{stdout}")
+
+    return returncode, stdout
