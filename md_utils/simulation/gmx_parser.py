fddcdf756b9b43af9e7040074bbd025d05961bbf
wehs7661 <wehs7661@colorado.edu>
2026-02-11 15:54:12 +0000

    Added a mdp parser

--- md_utils/simulation/gmx_parser.py
+++ md_utils/simulation/gmx_parser.py
@@ -40,3 +40,118 @@ def parse_ndx(ndx_file):
         group_str += f"  ({i}) {'':<{idx_w - len(str(i))}}{label:<{label_w}} {len(atoms):>{count_w}} atoms\n"
 
     return groups, group_str
+
+
+class MDP(odict):
+    """
+    A class that represents a GROMACS MDP file. Note that an MDP instance is an ordered dictionary,
+    with the i-th key corresponding to the i-th line in the MDP file. Comments and blank lines are
+    also preserved, e.g., with keys 'C0001' and 'B0001', respectively. The value corresponding to a
+    'C' key is the comment itself, while the value corresponding to a 'B' key is an empty string.
+    Comments after a parameter on the same line are discarded. Leading and trailing spaces
+    are always stripped.
+
+    Parameters
+    ----------
+    input_mdp : str, Optional
+        The path of the input MDP file. The default is None.
+    **kwargs : Optional
+        Additional keyword arguments to be passed to add additional key-value pairs to the MDP instance.
+        Note that no sanity checks will be performed for the key-value pairs passed in this way. This
+        also does not work for keys that are not legal python variable names, such as anything that includes
+        a minus '-' sign or starts with a number.
+
+    Attributes
+    ----------
+    COMMENT : :code:`re.Pattern` object
+        A compiled regular expression pattern for comments in MDP files.
+    PARAMETER : :code:`re.Pattern` object
+        A compiled regular expression pattern for parameters in MDP files.
+    input_mdp : str
+        The real path of the input MDP file returned by :code:`os.path.realpath(input_mdp)`,
+        which resolves any symbolic links in the path.
+
+    Example
+    -------
+    >>> from ensemble_md.utils import gmx_parser
+    >>> gmx_parser.MDP("em.mdp")
+    MDP([('C0001', 'em.mdp - used as input into grompp to generate em.tpr'), ('C0002', 'All unspecified parameters adopt their own default values.'), ('B0001', ''), ('C0003', 'Run Control'), ('integrator', 'steep'), ('nsteps', 500000), ('B0002', ''), ('C0004', 'Energy minnimization'), ('emtol', 100.0), ('emstep', 0.01), ('B0003', ''), ('C0005', 'Neighbor searching/Electrostatics/Van der Waals'), ('cutoff-scheme', 'Verlet'), ('nstlist', 10), ('ns_type', 'grid'), ('pbc', 'xyz'), ('coulombtype', 'PME'), ('rcoulomb', 1.0), ('rvdw', 1.0)])  # noqa: E501
+    """
+    # Below are some class variables accessible to all functions.
+    COMMENT = re.compile("""\s*;\s*(?P<value>.*)""")  # noqa: W605
+    PARAMETER = re.compile("""\s*(?P<parameter>[^=]+?)\s*=\s*(?P<value>[^;]*)(?P<comment>\s*;.*)?""", re.VERBOSE)  # noqa: W605, E501
+
+    def __init__(self, input_mdp=None, **kwargs):
+        super(MDP, self).__init__(**kwargs)  # can use kwargs to set dict! (but no sanity checks!)
+        if input_mdp is not None:
+            self.input_mdp = os.path.realpath(input_mdp)
+            self.read()
+
+    def read(self):
+        """
+        Reads and parses the input MDP file.
+        """
+        def BLANK(i):
+            return f"B{i:04d}"
+
+        def COMMENT(i):
+            return f"C{i:04d}"
+
+        data = odict()
+        iblank = icomment = 0
+        with open(self.input_mdp) as mdp:
+            for line in mdp:
+                line = line.strip()
+                if len(line) == 0:
+                    iblank += 1
+                    data[BLANK(iblank)] = ""
+                    continue
+                m = self.COMMENT.match(line)
+                if m:
+                    icomment += 1
+                    data[COMMENT(icomment)] = m.group("value")
+                    continue
+
+                m = self.PARAMETER.match(line)
+                if m:
+                    parameter = m.group("parameter")
+                    value = utils._convert_to_numeric(m.group("value"))
+                    data[parameter] = value
+                else:
+                    err_msg = f"{os.path.basename(self.input_mdp)!r}: unknown line in mdp file, {line!r}"
+                    raise ParseError(err_msg)
+
+        super(MDP, self).update(data)
+
+    def write(self, output_mdp=None, skipempty=False):
+        """
+        Writes the MDP instance (the ordered dictionary) to an output MDP file.
+
+        Parameters
+        ----------
+        output_mdp : str, Optional
+            The file path of the output MDP file. The default is the filename the MDP instance was built from.
+            If that if :code:`output_mdp` is not specified, the input MDP file will be overwritten.
+        skipempty : bool, Optional
+            Whether to skip empty values when writing the MDP file. If :code:`True`, any parameter lines from
+            the output that contain empty values will be removed. The default is :code:`False`.
+        """
+        # The line 'if skipempty and (v == "" or v is None):' below could possibly incur FutureWarning
+        warnings.simplefilter(action='ignore', category=FutureWarning)
+
+        if output_mdp is None:
+            output_mdp = self.input_mdp
+
+        with open(output_mdp, "w") as mdp:
+            for k, v in self.items():
+                if k[0] == "B":  # blank line
+                    mdp.write("\n")
+                elif k[0] == "C":  # comment
+                    mdp.write(f"; {v!s}\n")
+                else:  # parameter = value
+                    if skipempty and (v == "" or v is None):
+                        continue
+                    if isinstance(v, six.string_types) or not hasattr(v, "__iter__"):
+                        mdp.write(f"{k!s} = {v!s}\n")
+                    else:
+                        mdp.write(f"{k} = {' '.join(map(str, v))}\n")
